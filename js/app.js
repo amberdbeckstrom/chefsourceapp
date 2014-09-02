6189a200395dbe000e182d72f1d5dedb54206a9c
Amber Moss <amberdmoss@gmail.com>
2014-09-02 12:14:22 -0600

    ani fixed

--- js/app.js
+++ js/app.js
@@ -62,39 +62,39 @@ angular.module('website', ['ngAnimate', 'ngTouch'])
             $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
         };
     })
-    // .animation('.slide-animation', function () {
-    //     return {
-    //         beforeAddClass: function (element, className, done) {
-    //             var scope = element.scope();
+    .animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
 
-    //             if (className == 'ng-hide') {
-    //                 var finishPoint = element.parent().width();
-    //                 if(scope.direction !== 'right') {
-    //                     finishPoint = -finishPoint;
-    //                 }
-    //                 TweenMax.to(element, 0, {left: finishPoint, onComplete: done });
-    //             }
-    //             else {
-    //                 done();
-    //             }
-    //         },
-    //         removeClass: function (element, className, done) {
-    //             var scope = element.scope();
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
 
-    //             if (className == 'ng-hide') {
-    //                 element.removeClass('ng-hide');
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
 
-    //                 var startPoint = element.parent().width();
-    //                 if(scope.direction === 'right') {
-    //                     startPoint = -startPoint;
-    //                 }
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
 
-    //                 TweenMax.fromTo(element, 0, { left: startPoint }, {left: 0, onComplete: done });
-    //             }
-    //             else {
-    //                 done();
-    //             }
-    //         }
-    //     };
+                    TweenMax.fromTo(element, 0, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
     });
 
